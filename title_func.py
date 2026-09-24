439e977b7ae73b2364d711116b182b0d0d28a131
korovinaes <korovinaeks@gmail.com>
2026-09-24 17:59:36 +0400

    Update title_func.py
    Add upper func

--- title_func.py
+++ title_func.py
@@ -1,3 +1,7 @@
 def to_title(string: str) -> str:
     """Принимает строку и возвращает её с заглавной первой буквой каждого слова."""
     return string.title()
+
+def to_upper(string: str) -> str:
+    """Принимает строку и возвращает её со всеми заглавными буквами."""
+    return string.upper()
